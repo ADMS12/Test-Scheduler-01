fe45a3ac0f84be293ec6c0e48f1933f9322655f3
Durendal Huynh <durendalhuynh@gmail.com>
2013-12-08 00:56:21 -0600

    Changes made by Durendal Huynh (durendalhuynh@gmail.com).

--- Test Scheduler 01/scripts/weather.js
+++ Test Scheduler 01/scripts/weather.js
@@ -1,30 +1,3 @@
-(function (global) {
-    var WeatherViewModel,
-        app = global.app = global.app || {};
+ function initScheduler() {
 
-    WeatherViewModel = kendo.data.ObservableObject.extend({
-        weatherDataSource: null,
-
-        init: function () {
-            var that = this,
-                dataSource;
-
-            kendo.data.ObservableObject.fn.init.apply(that, []);
-
-            dataSource = new kendo.data.DataSource({
-                transport: {
-                    read: {
-                        url: "data/weather.json",
-                        dataType: "json"
-                    }
-                }
-            });
-
-            that.set("weatherDataSource", dataSource);
-        }
-    });
-
-    app.weatherService = {
-        viewModel: new WeatherViewModel()
-    };
-})(window);
+ }
